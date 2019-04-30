4f6ec42772d8cceebb198fcec36f2f1411389395
Vique (Victória) <48133441+viquefreitas@users.noreply.github.com>
2019-04-30 10:24:38 -0300

    Create 19ex.php

---
+++ 19ex.php
@@ -0,0 +1,20 @@
+<?php
+
+//A série de Fibonacci é formada pela sequência 0,1,1,2,3,5,8,13,21,34,55, [...] Faça um programa que
+//gere a série até que o valor seja maior que 500.
+
+print "Fibonacci (0 a +500)\n";
+
+$a1=0;
+$a2=1;
+$n=16;
+$c=1;
+
+while ($c <= $n){
+$a3 = $a1 + $a2; 
+print "$a1 \n";
+
+$a1=$a2;
+$a2=$a3;
+$c=$c + 1;
+}
